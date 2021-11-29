a988b37882540b654287cb888602b60fa9de7f32
suyeon-hong <bellahong95@gmail.com>
2021-11-29 16:52:11 +0900

    쿠키팝업 오류 수정

--- js/popup.js
+++ js/popup.js
@@ -1,23 +1,33 @@
-const popup = document.querySelector(".popup");
-const btnClose = popup.querySelector(".close");
-const isCookie = document.cookie.indexOf("popup=isdone");
-
-(isCookie == -1) ? popup.style.display = "block" : popup.style.display = "none";
-
-btnClose.addEventListener("click", e=>{
-    e.preventDefault();
-    let ischecked = popup.querySelector("input[type='checkbox']").checked;
-
-    if(ischecked) setCookie("popup", "isdone", 1);
-    popup.style.display = "none";
-});
-
-
-function setCookie(name, value, delay){
-    let today = new Date();
-    let date = today.getDate();
-    today.setDate(date + delay);
-    let dueDate = today.toGMTString();
+class CookiePopup{
+    constructor(selector, opt){
+        this.init(selector, opt);
+        this.bindingEvent();
+    }
+    init(selector, opt){
+        this.popup = document.querySelector(selector);
+        this.btnClose = this.popup.querySelector(opt.btnClose);
+        this.isCookie = document.cookie.indexOf(`${opt.name}=${opt.value}`);
+        this.name = opt.name;
+        this.value = opt.value;
+        this.delay = opt.delay;
+    }
+    bindingEvent(){
+        (this.isCookie == 0) ? this.popup.style.display = "none" : this.popup.style.display = "block";
     
-    document.cookie = `${name} = ${value}; expires = ${dueDate};`;
+        this.btnClose.addEventListener("click", e=>{
+            e.preventDefault();
+            let ischecked = this.popup.querySelector("input[type='checkbox']").checked;
+    
+            if(ischecked) this.setCookie();
+            this.popup.style.display = "none";
+        });
+    }
+    setCookie(){
+        let today = new Date();
+        let date = today.getDate();
+        today.setDate(date + this.delay);
+        let dueDate = today.toGMTString();
+        
+        document.cookie = `${this.name} = ${this.value}; expires = ${dueDate};`;
+    }
 }
