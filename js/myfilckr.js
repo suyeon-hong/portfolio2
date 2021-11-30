85dba122bd1341f6d1171575d6338129dc6bb513
suyeon-hong <bellahong95@gmail.com>
2021-11-30 15:36:07 +0900

    gallery페이지 디자인 적용

---
+++ js/myfilckr.js
@@ -0,0 +1,75 @@
+class Myflickr{
+    constructor(selector, opt){
+        if(!selector){
+            console.error("selector값은 필수입력값 입니다.");
+            return;
+        }
+        this.init(selector, opt);
+        this.bindingEvent();
+    }
+    init(selector, opt){
+        this.photoBox = document.querySelector(selector);
+        this.base = "https://www.flickr.com/services/rest/?";
+        this.method = "flickr.interestingness.getList";
+        this.api_key = opt.key;
+        this.format = "json";
+        this.per_page = opt.page;
+        this.url = `${this.base}method=${this.method}&api_key=${this.api_key}&format=${this.format}&per_page=${this.per_page}&nojsoncallback=1`;
+    }
+    bindingEvent(){
+        this.callData();
+    }
+    callData(){
+        fetch(this.url)
+        .then(data=>{
+            let result = data.json();
+            return result;
+        })
+        .then(json=>{
+            let items = json.photos.photo;
+            this.createList(items);
+        })
+    }
+    createList(items){
+        let htmls = "";
+        items.map(data=>{
+            htmls += `
+                <li class='item'>
+                    <div class='li_inner'>
+                        <a href='https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg'>
+                            <img src='https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_n.jpg'>
+                        </a>
+                        <h1>${data.title}</h1>
+                        <h2>${data.owner}</h2>
+                    </div>
+                </li>
+            `
+        });
+        this.photoBox.innerHTML = htmls;
+        this.loadImg();
+    }
+    loadImg(){
+        let imgs = this.photoBox.querySelectorAll("img");
+        let len = imgs.length;
+        let num = 0;
+
+        for(let el of imgs){
+            el.addEventListener("load", ()=>{
+                num++;
+                if(num === len){
+                    new Isotope(this.photoBox, {
+                        itemSelector: ".item",
+                        columnWidth: ".item",
+                        transitionDuration: ".5s"
+                    });
+                }
+            });
+        }
+    }
+}
+
+
+
+
+
+
