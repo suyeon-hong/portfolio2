82a24746696587a4e0f26b695c81476c32225cb7
suyeon-hong <bellahong95@gmail.com>
2021-12-03 19:53:28 +0900

    MyYoutube class 생성

--- js/youtube.js
+++ js/youtube.js
@@ -1,16 +1,48 @@
-const key = "AIzaSyBiHz7c5hPNxp0P81utsPpEVutfeuY-OR8";
-const playlistId = "PLbO44G2j_RJzSSLK5KKfW45QZY1QzUr0b";
-const num = 3;
+class MyYoutube{
+    constructor(selector, opt){
+        this.init(selector, opt);
+        this.bindingEvent();
+    }
+    init(selector, opt){
+        this.frame = document.querySelector(selector);
+        this.key = opt.key;
+        this.playlistId = opt.playlistId;
+        this.num = opt.num;
+    }
+    bindingEvent(){
+        const url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${this.key}&part=snippet&playlistId=${this.playlistId}&maxResults=${this.num}`
 
-const url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${key}&part=snippet&playlistId=${playlistId}&maxResults=${num}`
-
-fetch(url)
-.then(data=>{
-    return data.json();
-})
-.then(json=>{
-    const items = json.items;
-
-    console.log(items);
-    
-});
+        fetch(url)
+        .then(data=>{
+            return data.json();
+        })
+        .then(json=>{
+            const items = json.items;
+            let results = "";
+        
+            items.forEach(item=>{
+                let tit = item.snippet.title;
+                let desc = item.snippet.description;
+                const date = item.snippet.publishedAt.split("T")[0];
+                const imgSrc = item.snippet.thumbnails.standard.url;
+        
+                if(tit.length > 50) tit = tit.substr(0, 50) + "...";
+                if(desc.length > 150) desc = desc.substr(0, 150) + "...";
+        
+                results += `
+                    <article>
+                        <h1>${date}</h1>
+                        <div class="pic">
+                            <img src=${imgSrc}>
+                        </div>
+                        <div class="con>
+                            <h2>${tit}</h2>
+                            <p>${desc}</p>
+                        </div>
+                    </article>
+                `;
+            });
+            this.frame.innerHTML = results;
+        });
+    }
+}
