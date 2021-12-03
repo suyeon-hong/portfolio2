49937ca0abb1b60830067eb1465c545baaf00c62
suyeon-hong <bellahong95@gmail.com>
2021-12-03 20:41:07 +0900

    MyYoutube 팝업창 생성,삭제기능 추가

--- js/youtube.js
+++ js/youtube.js
@@ -4,12 +4,18 @@ class MyYoutube{
         this.bindingEvent();
     }
     init(selector, opt){
+        this.body = document.querySelector("body");
         this.frame = document.querySelector(selector);
         this.key = opt.key;
         this.playlistId = opt.playlistId;
         this.num = opt.num;
     }
     bindingEvent(){
+        this.getData();
+        this.frame.addEventListener("click", e=>this.createPop(e));
+        this.body.addEventListener("click", e=>this.removePop(e));
+    }
+    getData(){
         const url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${this.key}&part=snippet&playlistId=${this.playlistId}&maxResults=${this.num}`
 
         fetch(url)
@@ -26,23 +32,44 @@ class MyYoutube{
                 const date = item.snippet.publishedAt.split("T")[0];
                 const imgSrc = item.snippet.thumbnails.standard.url;
         
-                if(tit.length > 50) tit = tit.substr(0, 50) + "...";
+                if(tit.length > 30) tit = tit.substr(0, 30) + "...";
                 if(desc.length > 150) desc = desc.substr(0, 150) + "...";
         
                 results += `
                     <article>
                         <h1>${date}</h1>
                         <div class="pic">
-                            <img src=${imgSrc}>
-                        </div>
-                        <div class="con>
-                            <h2>${tit}</h2>
-                            <p>${desc}</p>
+                            <a href="${item.snippet.resourceId.videoId}"><img src=${imgSrc}></a>
+                            <div class="con">
+                                <h2>${tit}</h2>
+                                <p>${desc}</p>
+                            </div>
                         </div>
                     </article>
                 `;
             });
             this.frame.innerHTML = results;
         });
+    }
+    createPop(e){
+        e.preventDefault();
+
+        const pic = e.target.closest(".pic");
+        if (pic == null) return;
+
+        const vidId = pic.querySelector("a").getAttribute("href");
+        let pop = document.createElement("aside");
+        pop.innerHTML = `
+            <iframe src="https://www.youtube.com/embed/${vidId}" frameborder="0" allowfullscreen></iframe>
+            <span class="close">CLOSE</span>
+        `;
+        this.body.append(pop);
+    }
+    removePop(e){
+        const aside = e.target.closest("aside");
+        if(aside == null) return;
+        
+        const closeBtn = aside.querySelector(".close");
+        if(e.target == closeBtn) aside.remove();
     }
 }
