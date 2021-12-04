35d9f21cd08441dd4df8c6a8a8c6fc0f9469cddc
suyeon-hong <bellahong95@gmail.com>
2021-12-04 14:58:09 +0900

    메인 스크롤 마우스휠 이벤트 추가

--- js/scroll.js
+++ js/scroll.js
@@ -1,17 +1,16 @@
 const myscrolls = document.querySelectorAll(".myscroll");
 const navis = document.querySelectorAll("#navi li");
 const navis_arr = Array.from(navis);
-const base = -300;
 const speed = 500;
 let posArr;
 
 setPos();
 
 window.addEventListener("scroll", ()=>{
-    let scroll = window.scrollY || window.pageYOffset;
+    const scroll = window.scrollY || window.pageYOffset;
 
     myscrolls.forEach((_,index)=>{
-        if(scroll >= posArr[index] + base){
+        if(scroll >= posArr[index]){
             activation(navis, index);
             activation(myscrolls, index);
         }
@@ -19,11 +18,27 @@ window.addEventListener("scroll", ()=>{
 });
 
 window.addEventListener("resize", ()=>{
-    let itemIndex = document.querySelector("#navi li.on");
-    let activeIndex = navis_arr.indexOf(itemIndex);
+    const itemIndex = document.querySelector("#navi li.on");
+    const activeIndex = navis_arr.indexOf(itemIndex);
     setPos();
     movingScroll(activeIndex);
 });
+
+window.addEventListener("mousewheel", e=>{
+    e.preventDefault();
+
+    const itemIndex = document.querySelector("#navi li.on");
+    const activeIndex = navis_arr.indexOf(itemIndex);
+
+    if(e.deltaY > 0){
+        if(activeIndex == myscrolls.length - 1) return;
+        movingScroll(activeIndex + 1);
+    }
+    if(e.deltaY < 0){
+        if(activeIndex == 0) return;
+        movingScroll(activeIndex - 1);
+    }
+}, {passive:false});
 
 navis.forEach((navi,index)=>{
     navi.addEventListener("click", ()=>{
