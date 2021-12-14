0feee1606f515053738b5dbfbe2a183a74be569f
suyeon-hong <bellahong95@gmail.com>
2021-12-14 12:01:01 +0900

    cookie popup checkbox 디자인 수정

--- js/popup.js
+++ js/popup.js
@@ -6,6 +6,7 @@ class CookiePopup{
     init(selector, opt){
         this.popup = document.querySelector(selector);
         this.btnClose = this.popup.querySelector(opt.btnClose);
+        this.label = this.popup.querySelector("label");
         this.isCookie = document.cookie.indexOf(`${opt.name}=${opt.value}`);
         this.name = opt.name;
         this.value = opt.value;
@@ -20,6 +21,10 @@ class CookiePopup{
     
             if(ischecked) this.setCookie();
             this.popup.style.display = "none";
+        });
+
+        this.label.addEventListener("click", ()=>{
+            this.label.classList.toggle("on");
         });
     }
     setCookie(){
