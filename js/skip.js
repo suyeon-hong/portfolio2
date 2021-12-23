95200ce8708ca4919034a41e886df5f6d5a52ad6
suyeonhong <bellahong95@gmail.com>
2021-12-23 18:24:19 +0900

    skip navigation 추가

---
+++ js/skip.js
@@ -0,0 +1,10 @@
+const skip = document.querySelectorAll("#skip a");
+
+skip.forEach(a=>{
+    a.addEventListener("focusin", ()=>{
+        a.classList.add("on")
+    });
+    a.addEventListener("focusout", ()=>{
+        a.classList.remove("on")
+    });
+})
